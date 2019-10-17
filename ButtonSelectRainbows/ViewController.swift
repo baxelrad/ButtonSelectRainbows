f86642e335058f058558ce708339997b9ecb7304
Brett <consultant@Consultants-MacBook-Pro.local>
2019-10-17 09:30:03 -0400

    first commit

---
+++ ButtonSelectRainbows/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ButtonSelectRainbows
+//
+//  Created by Brett on 10/17/19.
+//  Copyright © 2019 Brett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var button: UIButton!
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        button.setTitle("REEEEE", for: .normal)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
