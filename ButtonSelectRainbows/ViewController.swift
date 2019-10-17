354b37771fdb2a2e4b662b8551f021e48540656c
Brett <baxelrad>
2019-10-17 09:53:02 -0400

    added rainbows

--- ButtonSelectRainbows/ViewController.swift
+++ ButtonSelectRainbows/ViewController.swift
@@ -13,8 +13,16 @@ class ViewController: UIViewController {
     
     @IBOutlet var button: UIButton!
     
+    func randomNum() -> CGFloat{
+        return CGFloat(Int.random(in: 0...255))
+    }
+    
     @IBAction func buttonAction(_ sender: Any) {
         button.setTitle("REEEEE", for: .normal)
+        //UIColor -> 0.0 - 1.0
+        let randomColor = UIColor(red: randomNum()/255.0, green: randomNum()/255.0, blue: randomNum()/255.0, alpha: 1.0)
+        
+        button.backgroundColor = randomColor
     }
     override func viewDidLoad() {
         super.viewDidLoad()
